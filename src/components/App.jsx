43125bc4075ae409588333c2dd520322553be82e
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-02-26 22:56:21 +0200

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,8 +14,7 @@ export class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
+ 
   };
 
   addContact = newContact => {
